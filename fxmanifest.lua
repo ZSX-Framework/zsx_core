65660094e7b90f02e412f825946edd9ee476cdea
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2023-11-06 08:59:07 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game {'gta5'}
+
+client_scripts {
+  'client/**/*.lu*',
+  'client/*.lu*',
+  'config.lu*'
+}
+
+server_scripts {
+  'server/**/*.lu*',
+  'server/*.lu*',
+  'config.lu*',
+  'config_sv.lu*'
+}
